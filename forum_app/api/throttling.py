bebe97df645815c677b6fe09e27afa87f9c45b55
Marius Kasparek <marius.kasparek@gmail.com>
2025-01-22 10:34:46 +0100

    improved QuestionThrottle class

--- forum_app/api/throttling.py
+++ forum_app/api/throttling.py
@@ -2,3 +2,16 @@ from rest_framework.throttling import UserRateThrottle
 
 class QuestionThrottle(UserRateThrottle):
     scope = 'question'
+
+    def allow_request(self, request, view):
+        if request.method == "GET":
+            return True
+        
+        new_scope = 'question-' + request.method.lower()
+
+        if new_scope in self.THROTTLE_RATES:
+            self.scope = new_scope
+            self.rate = self.get_rate()
+            self.num_requests, self.duration = self.parse_rate(self.rate)
+            
+        return super().allow_request(request, view)
--- stackunderflow/settings.py
+++ stackunderflow/settings.py
@@ -156,5 +156,10 @@ REST_FRAMEWORK = {
         'anon': '100/day',
         'user': '1000/day',
         'question': '10/day',
+        'question-post': '2/day',
+        'question-put': '2/day',
+        'question-patch': '2/day',
+        'question-delete': '2/day',
+        'question-options': '10/day'
     },
 }
