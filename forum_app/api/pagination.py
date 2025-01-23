5f5fabc07dd9e4fd08eb00e57b9b905fdc2a4e1c
Marius Kasparek <marius.kasparek@gmail.com>
2025-01-23 12:31:10 +0100

    added limit offset pagination

--- forum_app/api/pagination.py
+++ forum_app/api/pagination.py
@@ -1,7 +1,13 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
 
 class LargeRestulsSetPagination(PageNumberPagination):
     page_size = 100
     page_size_query_param = 'page_size'
     max_page_size = 200
+
+class CustomLimitOffSetPagination(LimitOffsetPagination):
+    default_limit = 10
+    limit_query_param = 'limit'
+    offset_query_param = 'offset'
+    max_limit = 100
